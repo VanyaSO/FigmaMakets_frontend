d19858133677401187d13b05337d9877d6a70132
Ivan Ushachov <ushachovg324@gmail.com>
2025-03-06 15:10:01 +0200

    chore: update linting and formatting setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            react: react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            '@typescript-eslint': tseslint,
+            prettier: prettier,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...tseslint.configs.recommended.rules,
+            ...prettierConfig.rules,
+            'prettier/prettier': ['error', { usePrettierrc: true }],
+            'react/react-in-jsx-scope': 'off',
+            'quotes': ['error', 'double', { avoidEscape: true }],
+            'jsx-quotes': ['error', 'prefer-double'],
+            'semi': ['error', 'always'],
+            'no-mixed-spaces-and-tabs': 'error',
+            'no-console': 'warn',
+            'eqeqeq': 'error',
+            'no-trailing-spaces': 'error',
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    {
+        ignores: ['dist'],
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+];
