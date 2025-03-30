be106ddd3c127a5911ce04715544a4eca84c14ad
Ivan Ushachov <ushachovg324@gmail.com>
2025-03-30 17:58:40 +0300

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {RegisterForm} from "./RegisterForm/RegisterForm";
+import {RegisterForm2} from "./RegisterForm2/RegisterForm2";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RegisterForm />
+    <RegisterForm2 />
   </StrictMode>,
 )
