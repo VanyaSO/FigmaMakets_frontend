72c0ee56d1b2453dc5ac38517d628ab233ecd979
Ivan Ushachov <ushachovg324@gmail.com>
2025-04-22 17:15:03 +0300

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import "./app/styles/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RegisterForm } from "./components/RegisterForm/RegisterForm";
-import "./assets/styles/index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./app/router/router";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RegisterForm />
+        <RouterProvider router={router} />
     </StrictMode>
 );
